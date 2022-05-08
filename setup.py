11233b92a095e7a34bdd43c83b834efecc7a7048
Hoang <42971570+wardetu@users.noreply.github.com>
2022-05-09 02:14:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup, find_packages
 
-
-with open('readme.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='mojito',
 
